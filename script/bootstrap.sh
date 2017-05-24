2eb6581686a4b0b5b7548310f66efa1515e6d34a
@slovacus <slovacus@gmail.com>
2017-05-24 15:21:21 -0500

    !#1 Add: scripts for makefile

---
+++ script/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+export PROJECT_NAME=python-example-graphene
+
+export PYTHON_VERSION=3.6.1
+export PYENV_NAME="${PROJECT_NAME}"
+
+export GRIP_PORT=6430
+
+export APP_IP=0.0.0.0
+export APP_PORT=8000
+
+# Vars Dir
+export ROOT_DIR
+ROOT_DIR=$(pwd)
+export RESOURCES_DIR="$ROOT_DIR/resources"
+export RESOURCES_DB_DIR="$RESOURCES_DIR/db"
+export PROVISION_DIR="$ROOT_DIR/provision/ansible"
+export SOURCE_DIR="${ROOT_DIR}/src/"
+export REQUIREMENTS_DIR="${SOURCE_DIR}/requirements/"
---
+++ script/runserver.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# shellcheck source=/dev/null
+[ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+
+cd "${SOURCE_DIR}" || echo 'Not Found'
+
+python manage.py runserver "${APP_IP}:${APP_PORT}"
