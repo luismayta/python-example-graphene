6b08918896ac1090add272d52f6344f628ae525f
@slovacus <slovacus@gmail.com>
2017-05-24 17:58:52 -0500

    !#2 Add: example hello

---
+++ src/hello.py
@@ -0,0 +1,21 @@
+import graphene
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(
+        name=graphene.Argument(
+            graphene.String,
+            default_value="stranger")
+    )
+
+    def resolve_hello(self, args, context, info):
+        return 'Hello {}'.format(
+            args.get('name')
+        )
+
+
+schema = graphene.Schema(query=Query)
+result = schema.execute('{ hello }')
+import ipdb
+ipdb.set_trace()
+print(result.data.get('hello'))
